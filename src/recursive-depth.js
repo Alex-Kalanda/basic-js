dff133a10a9c871f079be185f2485be150ad71f6
Alex-Kalanda <74773439+Alex-Kalanda@users.noreply.github.com>
2021-03-07 00:00:17 +0300

    feat: add solution >> what-season

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    calculateDepth(arr) {
+
+      throw new CustomError('Not implemented');
+      // remove line with error and write your code here
+        /*let func = (arr, level = 0) => {
+          if (arr instanceof Array) {
+                return arr.map(function (value, index) {
+                    return func(value, level + 1)
+                }).max()
+            }
+            return level;
+        }*/
+    }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function getSeason( date ) {
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (date == null) return 'Unable to determine the time of year!'
+  if (isNaN(date)) throw new Error()
+
+  let month = date.getMonth()
+  switch (month) {
+    case 11 : return 'winter'
+    case 0 : return 'winter'
+    case 1 : return 'winter'
+    case 2 : return 'spring'
+    case 3 : return 'spring'
+    case 4 : return 'spring'
+    case 5 : return 'summer'
+    case 6 : return 'summer'
+    case 7 : return 'summer'
+    case 8 : return 'autumn'
+    case 9 : return 'autumn'
+    case 10 : return 'autumn'
+    default:
+      return false
+  }
 };
